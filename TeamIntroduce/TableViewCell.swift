d6fdfbd2f3b2b56a27f5ecdc6354d354e5dadc12
soobeen27 <youdew27@gmail.com>
2024-05-30 11:27:44 +0900

    semi final(change main view and set storyboard reference)

--- TeamIntroduce/TableViewCell.swift
+++ TeamIntroduce/TableViewCell.swift
@@ -9,30 +9,16 @@ import UIKit
 
 class TableViewCell: UITableViewCell {
 
-    @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var positionLabel: UILabel!
-    
-    
+    @IBOutlet weak var profileImgView: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
+//        profileImgView.layer.cornerRadius = profileImgView.frame.height / 2
+        profileImgView.layer.cornerRadius = 15
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-    }
-    
-    func configure(team: Team) {
-        profileImage.image = UIImage(named: team.imageName)
-        profileImage.layer.cornerRadius = profileImage.bounds.width / 2
-        nameLabel.text = team.name
-        nameLabel.textColor = .white
-        positionLabel.text = team.position
-        if team.position == "팀장" {
-            positionLabel.textColor = .orange
-        } else {
-            positionLabel.textColor = .darkGray
-        }
     }
 
 }
--- TeamIntroduce/ViewController.swift
+++ TeamIntroduce/ViewController.swift
@@ -8,18 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var stackView: UIStackView!
-    @IBOutlet weak var JSJImageView: UIImageView!
-    @IBOutlet weak var KYHImageView: UIImageView!
-    
+    @IBOutlet weak var tableView: UITableView!
+    let teamName: [TeamName] = [
+    TeamName(id: "JSJ", name: "전상진"),
+    TeamName(id: "KYH", name: "김윤홍"),
+    TeamName(id: "JSB", name: "장수빈"),
+    TeamName(id: "YDS", name: "윤대성"),
+    TeamName(id: "AJH", name: "안지현"),
+    TeamName(id: "KDJ", name: "김동준"),
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        JSJImageView.layer.cornerRadius = JSJImageView.bounds.width / 2
-        KYHImageView.layer.cornerRadius = KYHImageView.bounds.width / 2
-        
-        for subview in stackView.arrangedSubviews {
-            subview.layer.cornerRadius = 10
+        tableView.dataSource = self
+        tableView.delegate = self
         }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return teamName.count
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell") as! TableViewCell
+        cell.nameLabel.text = teamName[indexPath.row].name
+        cell.profileImgView.image = UIImage(named: teamName[indexPath.row].id)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: teamName[indexPath.row].id, sender: self)
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
+}
+
+struct TeamName {
+    let id: String
+    let name: String
 }
